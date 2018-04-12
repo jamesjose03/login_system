37a683f06c799f9b5a6ef5e79aa609933c1b426c
James Kaviyil Jose <jamesjose03@gmail.com>
2018-04-12 23:36:50 +0530

    Basic UI implementation done

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login System</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+             <div class="main-wrapper">
+                 <ul>
+                     <li> <a href="index.php"> <b>MindShare</b> </a></li>
+                     
+                 </ul>
+                 <div class= "nav-login">
+                     <form>
+                     <input type="text" name="user_username" placeholder="Username/Email">
+                     <input type="password" name="user_password" placeholder="Password">
+                     <button type="submit" name ="submit">Login</button>
+                    
+                    
+                    </form>
+                    <a href="signup.php"> Sign Up </a>
+                 </div>
+             </div>
+        </nav>
+    </header>
---
+++ signup.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'header.php'; 
+?>
+
+    <section class="main-container">
+        <div class="main-wrapper">
+            <h2> Signup </h2>
+        </div>
+    </section>
+
+<?php
+include_once 'footer.php'; 
+?>
+
